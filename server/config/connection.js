5c0a1d7415122883e8e2fc9f1414050db938b1e8
DrMoE77 <drmo.afifi@gmail.com>
2022-11-18 10:16:13 +1100

    deploy

---
+++ server/config/connection.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://DrMoE77:Yasseen2015@cluster0.qusi2.mongodb.net/priclesschemist?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+});
+
+module.exports = mongoose.connection;
